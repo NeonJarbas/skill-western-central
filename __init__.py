24b0c78dbaf4b5e75366bc8c52e1ac494fd4aac7
jarbasai <jarbasai@mailfence.com>
2023-01-06 15:18:33 +0000

    scrap automation

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from os.path import join, dirname
-
+import random
 from ovos_plugin_common_play.ocp import MediaType, PlaybackType
 from ovos_utils.log import LOG
 from ovos_utils.parse import fuzzy_match
@@ -22,12 +22,14 @@ class WesternCentralSkill(OVOSCommonPlaybackSkill):
                                       logger=LOG)
 
     def initialize(self):
-        url = "https://www.youtube.com/channel/UCmXQRtLSFJC4v_pYXaM3DGQ"
-        bootstrap = "https://raw.githubusercontent.com/OpenJarbas/streamindex/main/WesternCentral.json"
+        bootstrap = "https://github.com/JarbasSkills/skill-western-central/raw/dev/bootstrap.json"
         self.archive.bootstrap_from_url(bootstrap)
-        self.archive.monitor(url)
-        self.archive.setDaemon(True)
-        self.archive.start()
+        self.schedule_event(self._sync_db, random.randint(3600, 24 * 3600))
+
+    def _sync_db(self):
+        url = "https://www.youtube.com/channel/UCmXQRtLSFJC4v_pYXaM3DGQ"
+        self.archive.parse_videos(url)
+        self.schedule_event(self._sync_db, random.randint(3600, 24*3600))
 
     # matching
     def match_skill(self, phrase, media_type):
